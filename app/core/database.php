60518b95180c56a72369f4e3a6e738b59400b430
redel-byte <alhabibridouane@gmail.com>
2026-01-20 23:36:10 +0100

    making the connection between the aiven db and the project with seccussing in login and logout

--- app/core/database.php
+++ app/core/database.php
@@ -1,27 +1,36 @@
 <?php
 
 namespace App\Core;
+use PDO;
+use Pdo\Mysql;
+use Dotenv\Dotenv;
+Dotenv::createImmutable(__DIR__ . '/../../')->load();
 
 class Database
 {
-  private static ?\PDO $conn = null;
+    private static ?\PDO $conn = null;
 
-  public static function connection():\PDO{
-    if(self::$conn === null)
+    public static function connection(): \PDO
     {
-      $config = require_once "dbconfig.php";
-      $dsn = "mysql:host={$config['host']};dbname={$config['database']};charset=utf8mb4";
-      try{
-      self::$conn = new \PDO($dsn,$config['user'],$config['password'],[
-      \PDO::ATTR_ERRMODE=>\PDO::ERRMODE_EXCEPTION,
-      \PDO::ATTR_DEFAULT_FETCH_MODE=>\PDO::FETCH_ASSOC,
-      \PDO::ATTR_EMULATE_PREPARES=>false,
-      \PDO::ATTR_PERSISTENT=>true,
-        ]);
-      }catch(\PDOException $e){
-      die("[!] Connection Failed\n".$e->getMessage());
-      }
+        if (self::$conn === null) {
+            $dsn = "mysql:host={$_ENV['HOST']};port={$_ENV['PORT']};dbname={$_ENV['DBNAME']};charset=utf8mb4";
+            
+            try {
+                self::$conn = new \PDO(
+                    $dsn,
+                    $_ENV['USER'],
+                    $_ENV['PASSWORD'],
+                    [
+                      PDO::ATTR_ERRMODE                  => PDO::ERRMODE_EXCEPTION,
+                      PDO::ATTR_DEFAULT_FETCH_MODE       => PDO::FETCH_ASSOC,
+                      PDO::ATTR_EMULATE_PREPARES         => false,
+                      Mysql::ATTR_SSL_CA                 => $_ENV['SSLCA'],
+                      Mysql::ATTR_SSL_VERIFY_SERVER_CERT => false,
+          ]);
+            } catch (\PDOException $e) {
+                die("[!] Connection Failed\n" . $e->getMessage());
+            }
+        }
+        return self::$conn;
     }
-    return self::$conn;
-  }
 }
